baf803423ab49ff3f5fae9de3155d729a5c9c434
Tuan Anh <tuan73176@gmail.com>
2024-03-20 15:51:56 +0700

    kebab: Drop overlay-lineage
    * We are building Statix

--- device.mk
+++ device.mk
@@ -27,10 +27,7 @@ PRODUCT_PACKAGES += \
     fstab.qcom \
     fstab.qcom.ramdisk
 
-# Overlays
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
-
+# Overlay
 PRODUCT_PACKAGES += \
     OPlusFrameworksResTarget \
     OPlusSettingsProviderResTarget \
